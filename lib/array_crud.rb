c0f8e675eb0ecb92b068e4d802aaf5af7f7cf205
Felipe Dolago <lagofelipe@gmail.com>
2020-07-26 20:34:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,9 +9,9 @@ end
 def add_element_to_end_of_array(array, element)
 array << element
 end
-array.unshift(element)
-def add_element_to_start_of_array(array, element)
 
+def add_element_to_start_of_array(array, element)
+array.shift(element)
 end
 
 def remove_element_from_end_of_array(array)
